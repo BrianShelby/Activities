639a16a9defebcab8be806113fe2bbc595bb01b3
Brian Shelby <bsshel@yahoo.com>
2022-06-15 17:21:52 -0500

    Adding activities 2.1, 2.2, 2.3, 2.4

---
+++ Week2/2.2.4-JSMorePrimitives.js
@@ -0,0 +1,47 @@
+// create five variables and assign them values
+// each variable should reference a different primitive data type
+// REMEMBER: you should never DELIBERATELY assign "undefined" to a variable
+
+let firstName = "Jack";
+let age = 5;
+let enrolledInSchool = true;
+let ethnicity
+let height = null;
+
+
+// print the type of each variable in this order:
+// string, number, boolean, undefined, null
+
+// let firstName = "Jack";
+// console.log(typeof firstName);
+// let age = 5;
+// console.log(typeof age);
+// let enrolledInSchool = true;
+// console.log(typeof enrolledInSchool);
+// let ethnicity
+// console.log(typeof ethnicity);
+// let height = null;
+// console.log(typeof height);
+
+// create a variable that references a template literal
+// inside the template literal, use two of the above variables
+
+// let greeting = `Hello ${firstName}, are you ${age} years old?`;
+// console.log(greeting)
+
+
+// reassign the value of the variable that references "null"
+
+height = "125cm";
+console.log(typeof height)
+
+// print the value and its type
+string
+
+// print a variable that causes a ReferenceError
+console.log(typeof dog);
+// alter the previous line to no longer cause a ReferenceError
+let dog = "Max";
+console.log(typeof dog);
+
+
