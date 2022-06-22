bf7b7a44097256eca163e25d0619de81f559f421
Brian Shelby <bsshel@yahoo.com>
2022-06-22 18:18:09 -0500

    Update Activities Week 3

---
+++ Week3/3.1.2-FunctionInvocation.js
@@ -0,0 +1,46 @@
+// print the function below
+function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  console.log(num);
+}
+
+// now, invoke the myNumberFunction function
+
+myNumberFunction();
+
+// print just the function of console.log (no invocation)
+console.log(console.log);
+
+
+
+
+
+
+/*
+  3.1.2-FunctionInvocation#index.js
+  =================================
+*/
+
+// print the function below
+
+function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  console.log(num);
+}
+console.log(myNumberFunction);
+
+
+// Now, invoke the myNumberFunction function
+// myNumberFunction();
+
+console.log(myNumberFunction());
+
+
+// Print just the function of console.log (no invocation)
+console.log(console.log);
---
+++ Week3/3.1.3-ReturnsAndShortCircuiting.js
@@ -0,0 +1,94 @@
+// ? what does the function below return?
+function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  console.log(num);
+}
+
+// change the function above to return a value
+
+// Short Circuiting
+// ! the console.log will not print
+function shortCircuitFunction() {
+  return "Zap! - Short Circuited!";
+  console.log(
+    "This will never print because it is after the return statement."
+  );
+}
+
+// ? will the following function short circuit?
+let height = 50;
+function checkCircusRides() {
+  if (height < 48) {
+    console.log("Take a ride on the carousel!");
+  }
+  if (height < 60) {
+    console.log("Try the bumper cars!");
+  }
+  console.log("Enjoy the roller coaster!");
+}
+
+// change the function above to use short circuiting
+
+
+
+
+
+
+
+/*
+  3.1.3-ReturnsAndShortCircuiting#index.js
+  =========================================
+*/
+
+// ? what does the function below return?
+function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  return num;
+}
+ // myNumberFunction();
+
+
+// change the function above to return a value
+
+
+//  Change the return statement to...
+// return console.log(num);
+
+
+// Short Circuiting
+// ! the console.log will not print
+function shortCircuitFunction() {
+  return "Zap! - Short Circuited!";
+  console.log(
+    "This will never print because it is after the return statement."
+  );
+}
+ // shortCircuitFunction();
+
+//  Now try uncommenting the next line
+// console.log(shortCircuitFunction());
+
+
+// ? will the following function short circuit?
+let height = 33;
+function checkCircusRides() {
+  if (height < 48) {
+    return "Take a ride on the carousel!";
+  }
+  if (height < 60) {
+    return "Try the bumper cars!";
+  }
+  return "Enjoy the roller coaster!";
+};
+checkCircusRides();
+console.log(checkCircusRides());
+
+// change the function above to use short circuiting
+
+// To short circuit, add a Return before any one of the IF statements, or the console.log
